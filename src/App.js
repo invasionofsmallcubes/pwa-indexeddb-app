64d61c6925de28490cb54ce4fd15b435309eb941
invasionofsmallcubes <dierre@gmail.com>
2020-03-01 21:07:27 +0100

    Revert "added service worker update"
    This reverts commit 5d979cb34a46809159bb4229d4feb870f48365b8.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 import ExternalLink from './ExternalLink';
-import { useServiceWorker } from './ServiceWorkerContext';
-
 
 function App() {
-  const { isUpdateAvailable, updateAssets } = useServiceWorker();
-
   return (
     <div className="App">
       <header className="App-header">
@@ -15,12 +11,7 @@ function App() {
         </p>
         <p>It's a simple experiment to see if <ExternalLink link="https://web.dev/progressive-web-apps/" name="Progressive Web Apps" /> and <ExternalLink link="https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API" name="IndexedDb" /> work together.</p>
       </header>
-      {isUpdateAvailable && (
-        <div>
-          A new version of this app is available!
-          <button type="button" onClick={updateAssets}>Update now</button>
-        </div>
-      )}
+      
     </div>
   );
 }
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -58,11 +58,6 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
-      if (registration.waiting) {
-        if (config && config.onWaiting) {
-          config.onWaiting(registration.waiting);
-        }
-      }
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
