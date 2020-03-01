fdf1e9f0bdf145f3752ad89d889b3b654dc87dec
invasionofsmallcubes <dierre@gmail.com>
2020-03-01 18:51:19 +0100

    version 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello, welcome to <strong>pwa-indexeddb-app</strong> 2.
+          Hello, welcome to <strong>pwa-indexeddb-app</strong> 3.
         </p>
         <p>It's a simple experiment to see if <ExternalLink link="https://web.dev/progressive-web-apps/" name="Progressive Web Apps" /> and <ExternalLink link="https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API" name="IndexedDb" /> work together.</p>
       </header>
