2c64d01ce6e190a823ba4783bd0803a0dea02b1c
Emanuele Ianni <emanuele.ianni@maersk.com>
2020-03-11 17:44:53 +0100

    cristo

--- src/CreateTodo.test.js
+++ src/CreateTodo.test.js
@@ -8,11 +8,11 @@ test('I can create a Todo clicking the button', async () => {
     const success = Promise.resolve(todo);
     const historyResolve = Promise.resolve();
 
-    const hp1 = historyProps(historyResolve);
+    const hp = historyProps(historyResolve);
 
     const { getByLabelText, getByText } = render(<CreateTodo
         todoRepository={asyncTodoRepository(success)}
-        history={hp1} />);
+        history={hp} />);
 
     const input = getByLabelText(/Title/i);
     const button = getByText(/Create/i);
@@ -23,7 +23,7 @@ test('I can create a Todo clicking the button', async () => {
     await success;
     await historyResolve;
 
-    expect(hp1.push.mock.calls[0].length).toBe(1);
-    expect(hp1.push.mock.calls[0][0]['pathname']).toBe('/todos/' + todoId);
-    expect(hp1.push.mock.calls[0][0]['state'].todo).toMatchObject(todo);
+    expect(hp.push.mock.calls[0].length).toBe(1);
+    expect(hp.push.mock.calls[0][0]['pathname']).toBe('/todos/' + todoId);
+    expect(hp.push.mock.calls[0][0]['state'].todo).toMatchObject(todo);
 });
