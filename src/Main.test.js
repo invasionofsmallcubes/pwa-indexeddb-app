b177ae155352a6e7df3bee8d296195adb706fd20
Emanuele Ianni <emanuele.ianni@maersk.com>
2020-03-11 17:40:30 +0100

    fix history props everywhere

--- src/Main.test.js
+++ src/Main.test.js
@@ -9,12 +9,14 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('when I click on create new, I go on the component where I can create a todo', () => {
-
-  const { getByText } = render(<Main history={historyProps} />);
+test('when I click on create new, I go on the component where I can create a todo', async () => {
+  const success = Promise.resolve();
+  const hp = historyProps(success);
+  const { getByText } = render(<Main history={hp} />);
   const linkElement = getByText(/Create New ToDo/i);
   fireEvent.click(linkElement);
+  await success
 
-  expect(historyProps.push.mock.calls.length).toBe(1);
-  expect(historyProps.push.mock.calls[0][0].pathname).toBe('/todos/new');
+  expect(hp.push.mock.calls.length).toBe(1);
+  expect(hp.push.mock.calls[0][0].pathname).toBe('/todos/new');
 })
